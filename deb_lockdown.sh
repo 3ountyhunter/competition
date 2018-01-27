2663d766a112ede6fe82d5874044132d4dade679
3ountyhunter <35637752+3ountyhunter@users.noreply.github.com>
2018-01-27 03:12:06 -0500

    Added Paranoia
    Allowed for them setting immutable mode.  Set to reinstall essential utils.  Changed file permissions

--- deb_lockdown.sh
+++ deb_lockdown.sh
@@ -1,33 +1,41 @@
 # Assembled by NetHunter for CCDN 2017
 #!/bin/bash
-/bin/clear
+/usr/bin/clear
 if [ $EUID -ne 0 ]; then
    /bin/echo "This script must be run as root" 1>&2
 exit
 fi
 # Sets constants used for coloring output
+
 # Syntax echo "${red}red text ${green}green text${reset}"
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 /bin/echo "${green}Running Lockdown Script..."
-sleep 5
+/bin/sleep 5
 
 # Remove bashrc
 /bin/echo "Archiving files"
-sleep 3
+/bin/sleep 3
+/usr/bin/chattr -i ~/.profile*
 /bin/mkdir ~/original
 /bin/mv ~/.bash* ~/original
-/bin/cp ~/competition/.bashrc ~
-. ~/.bashrc
+/bin/mv ~/.profile* ~/original
+
+# Uncomment for paranoid mode
+/usr/bin/apt-get --reinstall install -y passwd
+/usr/bin/apt-get --reinstall install -y bash
+/usr/bin/apt-get --reinstall install -y coreutils
+/usr/bin/apt-get --reinstall install -y nano
 
 # Change password for root account
   echo "Input New Password for Root Account:"
-/usr/bin/passwd
+passwd
 
 # Fix file repositories
   echo "Fixing Repositories..."
   sleep 3
+  chattr -i /etc/apt/sources.list
 	cp /etc/apt/sources.list ~/original/sources.list.org
   VERSION=$(lsb_release -sc)
   echo "deb http://ftp.us.debian.org/debian/ $VERSION main"  > /etc/apt/sources.list
@@ -37,24 +45,47 @@ sleep 3
   echo "deb-src http://security.debian.org/debian-security $VERSION/updates main" >> /etc/apt/sources.list
 
 # Install needed Software
-
   echo "Installing Software..."
 sleep 3
 apt install glances ufw openssh-server -y
+
 # Check for multiple users with UID 0
 
 
 # Reset crontab
   echo -e "Resetting Crontab..."
 sleep 3
-cp -r /var/spool/cron ~/original
+# Remove protection and archive cronjobs
+chattr -i /etc/crontab
+chattr -i /var/spool/cron
+mv /var/spool/cron ~/original
 crontab -r
+
+# ----------------------Assign Permissions------------------ #
+chown root:root /etc/passwd-
+chmod 600 /etc/passwd-
+chown root:root /etc/security/opasswd
+chmod 600 /etc/security/opasswd
+chown root:root /etc/shadow-
+chmod 600 /etc/shadow-
+chown root:root /etc/group-
+chmod 600 /etc/group-
+chown root:shadow /etc/shadow
+chmod 640 /etc/shadow
+chown root:root /etc/gshadow-
+chmod 600 /etc/gshadow-
+
+# Uncomment for Extreme Paranoia (Warning: May Break System)
+chown -R root /etc
+
+# Allow only Root Cron
 cd /etc/
+chattr -i cron.*
 mv cron.deny cron.allow ~/original # at.deny
   echo root > cron.allow
 #echo root >at.allow (Might not apply to newer Debian)
 /bin/chown root:root cron.allow # at.allow
-/bin/chmod 640 cron.allow # at.allow
+/bin/chmod 600 cron.allow # at.allow
 clear
 
 #----------------Remote Administration----------------#
@@ -75,14 +106,14 @@ clear
 
 # Change SSH Config
 cp /etc/ssh/sshd_config ~/original
-sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
-sed -i "s/X11Forwarding yes/X11Forwarding no/g" /etc/ssh/sshd_config
-sed -i "s/*MaxAuthTries */MaxAuthTries 1/g" /etc/ssh/sshd_config
+sed -i "s/.*PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+sed -i "s/.*X11Forwarding.*/X11Forwarding no/g" /etc/ssh/sshd_config
+sed -i "s/.*MaxAuthTries.*/MaxAuthTries 1/g" /etc/ssh/sshd_config
   echo -e "${reset}Would you like to \e[31mdisable ${reset}SSH password authentication?"
 read ans
  if [ "$ans" = "Y" ] || [ "$ans" = "y" ];
  then
-   sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+   sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
 else
   echo "Script is Done, Good Luck!${reset}"
  fi
